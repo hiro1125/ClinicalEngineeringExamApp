a3d33556f79cd130c2b50ef01c0f5339ed6f25ec
hiro1125 <hiro112538@gmail.com>
2023-04-04 21:03:22 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>臨床工学技士国家試験</Text>
       <StatusBar style="auto" />
     </View>
   );
