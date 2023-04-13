c0cb7323176a1f06e6bd77a5ba0261c51af8690e
hiro1125 <hiro112538@gmail.com>
2023-04-13 18:34:10 +0900

    feat: 各分野ごとの画面遷移を追加

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -34,6 +34,51 @@ export const RootNavigator = () => {
         component={NinetyQuestionTestScreen}
         options={{ headerShown: false }}
       />
+      <Stack.Screen
+        name='IntroductionToMedicine'
+        component={IntroductionToMedicineScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='ClinicalMedicineOverview'
+        component={ClinicalMedicineOverviewScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='BiomedicalMeasurementInstrumentation'
+        component={BiomedicalMeasurementInstrumentationScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='MedicalTherapeuticEquipment'
+        component={MedicalTherapeuticEquipmentScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='MedDevSafety'
+        component={MedDevSafetyScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='MedicalElectricalAndElectronicEngineering'
+        component={MedicalElectricalAndElectronicEngineeringScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='BiologicalFunctionSubstitutionEquipment'
+        component={BiologicalFunctionSubstitutionEquipmentScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='MedicalMechanicalEngineering'
+        component={MedicalMechanicalEngineeringScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='BiologicalMaterialScienceAndEngineering'
+        component={BiologicalMaterialScienceAndEngineeringScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 };
@@ -74,6 +119,122 @@ export const NinetyQuestionTestScreen = ({ navigation }: Props) => {
   return (
     <AppScreen
       title='90問試験'
+      buttons={questionTestButton}
+      navigation={navigation}
+      textStyle={styles.examination}
+    />
+  );
+};
+
+/** IntroductionToMedicine画面 */
+export const IntroductionToMedicineScreen = ({ navigation }: Props) => {
+  return (
+    <AppScreen
+      title='医学概論'
+      buttons={questionTestButton}
+      navigation={navigation}
+      textStyle={styles.examination}
+    />
+  );
+};
+
+/** ClinicalMedicineOverview画面 */
+export const ClinicalMedicineOverviewScreen = ({ navigation }: Props) => {
+  return (
+    <AppScreen
+      title='臨床医学総論'
+      buttons={questionTestButton}
+      navigation={navigation}
+      textStyle={styles.examination}
+    />
+  );
+};
+
+/** BiomedicalMeasurementInstrumentation画面 */
+export const BiomedicalMeasurementInstrumentationScreen = ({
+  navigation,
+}: Props) => {
+  return (
+    <AppScreen
+      title='生体計測装置学'
+      buttons={questionTestButton}
+      navigation={navigation}
+      textStyle={styles.examination}
+    />
+  );
+};
+
+/** MedicalTherapeuticEquipment画面 */
+export const MedicalTherapeuticEquipmentScreen = ({ navigation }: Props) => {
+  return (
+    <AppScreen
+      title='医用治療機器学'
+      buttons={questionTestButton}
+      navigation={navigation}
+      textStyle={styles.examination}
+    />
+  );
+};
+
+/** MedDevSafety画面 */
+export const MedDevSafetyScreen = ({ navigation }: Props) => {
+  return (
+    <AppScreen
+      title='医用機器安全管理学'
+      buttons={questionTestButton}
+      navigation={navigation}
+      textStyle={styles.examination}
+    />
+  );
+};
+
+/** MedicalElectricalAndElectronicEngineering画面 */
+export const MedicalElectricalAndElectronicEngineeringScreen = ({
+  navigation,
+}: Props) => {
+  return (
+    <AppScreen
+      title='医用電気電子工学'
+      buttons={questionTestButton}
+      navigation={navigation}
+      textStyle={styles.examination}
+    />
+  );
+};
+
+/** BiologicalFunctionSubstitutionEquipment画面 */
+export const BiologicalFunctionSubstitutionEquipmentScreen = ({
+  navigation,
+}: Props) => {
+  return (
+    <AppScreen
+      title='生体機能代行装置学'
+      buttons={questionTestButton}
+      navigation={navigation}
+      textStyle={styles.examination}
+    />
+  );
+};
+
+/** MedicalMechanicalEngineering画面 */
+export const MedicalMechanicalEngineeringScreen = ({ navigation }: Props) => {
+  return (
+    <AppScreen
+      title='医用機械工学'
+      buttons={questionTestButton}
+      navigation={navigation}
+      textStyle={styles.examination}
+    />
+  );
+};
+
+/** BiologicalMaterialScienceAndEngineering画面 */
+export const BiologicalMaterialScienceAndEngineeringScreen = ({
+  navigation,
+}: Props) => {
+  return (
+    <AppScreen
+      title='生体物性材料工学'
       buttons={questionTestButton}
       navigation={navigation}
       textStyle={styles.examination}
