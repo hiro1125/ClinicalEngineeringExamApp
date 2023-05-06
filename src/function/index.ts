70270b8391773e980a16943ea007baa298c6f662
hiro1125 <hiro112538@gmail.com>
2023-05-06 17:41:12 +0900

    feat: 正解と不正解の効果音を追加

--- src/function/index.ts
+++ src/function/index.ts
@@ -1,18 +1,35 @@
 import { QuizAlertProps } from '../../types/type';
 import { Alert } from 'react-native';
+import { AVPlaybackSource, Audio } from 'expo-av';
 
-export const showCorrectAnswerAlert = ({
+const correctSound = require('../../assets/sounds/correct.mp3');
+const inCorrectSound = require('../../assets/sounds/incorrect.mp3');
+
+const playSound = async (sound: AVPlaybackSource) => {
+  try {
+    const soundObject = new Audio.Sound();
+    await soundObject.loadAsync(sound);
+    await soundObject.playAsync();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const showCorrectAnswerAlert = async ({
   score,
   setScore,
   setShowResultScreen,
   shuffledQuestions,
   index,
+  setIndex,
 }: QuizAlertProps) => {
+  await playSound(correctSound);
   Alert.alert('正解', 'おめでとうございます！', [
     {
       text: 'OK',
       onPress: () => {
         if (index < shuffledQuestions.length - 1) {
+          setIndex(index + 1);
           setScore(score + 1);
         } else {
           setShowResultScreen(true);
@@ -22,7 +39,7 @@ export const showCorrectAnswerAlert = ({
   ]);
 };
 
-export const showIncorrectAnswerAlert = ({
+export const showIncorrectAnswerAlert = async ({
   shuffledQuestions,
   index,
   setIndex,
@@ -31,6 +48,7 @@ export const showIncorrectAnswerAlert = ({
   const currentQuestion = shuffledQuestions[index];
   const correctAnswer = currentQuestion.options[currentQuestion.answerIndex];
   const message = `残念！正解は${correctAnswer}でした。`;
+  await playSound(inCorrectSound);
   Alert.alert('不正解', message, [
     {
       text: 'OK',
