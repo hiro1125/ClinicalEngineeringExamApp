a32f3772e7869054faf681f54a51223f837adc40
hiro1125 <hiro112538@gmail.com>
2023-05-13 22:00:40 +0900

    refactor: QuizQuestionCardコンポーネントのコードを簡潔化

---
+++ src/screens/QuizQuestionCard.tsx
@@ -0,0 +1,154 @@
+import React, { FC, useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { QuizData, RouteButton } from '../../types/type';
+import { useNavigation } from '@react-navigation/native';
+import { examMenuButton } from '../contents';
+
+type QuizQuestionCardProps = {
+  shuffledQuestions: Array<QuizData>;
+  index: number;
+  timer: number;
+  answerButton: (index: number) => void;
+};
+
+export const QuizQuestionCard: FC<QuizQuestionCardProps> = ({
+  shuffledQuestions,
+  index,
+  answerButton,
+}) => {
+  const [timer, setTimer] = useState<number>(10);
+
+  const min = Math.floor(timer / 60);
+  const rem = timer % 60;
+  const navigation = useNavigation();
+  const handleNavigation = (navigationName: string) => {
+    navigation.navigate(navigationName as never);
+  };
+  const byFieldMenuButtons = examMenuButton.filter(
+    (button: RouteButton) => button.navigationName === 'ByField'
+  );
+
+  useEffect(() => {
+    if (timer > 0) {
+      const interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [timer]);
+
+  return (
+    <View>
+      <View style={styles.returnContainer}>
+        {byFieldMenuButtons.map((button, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.byFieldButton}
+            onPress={() => handleNavigation(button.navigationName)}
+          >
+            <Text style={styles.playButtonText}>戻る</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <View style={styles.titleQuestion}>
+        <Text style={styles.questionNumber}>
+          【問題{index + 1} / {shuffledQuestions.length}】
+        </Text>
+        <Text style={styles.questionText}>
+          {shuffledQuestions[index].question}
+        </Text>
+      </View>
+      <Text style={styles.timeRestart}>
+        {min === 0 ? `残り時間：${rem}秒` : `残り時間：${min}分${rem}秒`}
+      </Text>
+      {shuffledQuestions[index].options.map((item, index) => {
+        return (
+          <TouchableOpacity
+            key={index}
+            onPress={() => answerButton(index)}
+            style={styles.answerButton}
+          >
+            <Text style={styles.buttonText}>{item}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+export default QuizQuestionCard;
+
+const styles = StyleSheet.create({
+  returnContainer: {
+    alignItems: 'flex-start',
+    marginLeft: 10,
+  },
+  byFieldButton: {
+    backgroundColor: '#2d82a8',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+  },
+  playButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  questionNumber: {
+    textAlign: 'center',
+    position: 'absolute',
+    top: 5,
+    margin: 5,
+  },
+  titleQuestion: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    width: 360,
+    height: 205,
+    borderRadius: 15,
+    backgroundColor: 'white',
+    fontSize: 15,
+    textAlign: 'center',
+    textTransform: 'capitalize',
+    shadowColor: '#aaa',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    marginBottom: 20,
+    margin: 10,
+  },
+  questionText: {
+    fontSize: 15,
+    textAlign: 'center',
+    padding: 5,
+  },
+  timeRestart: {
+    marginLeft: 10,
+    marginRight: 10,
+    shadowRadius: 6,
+    backgroundColor: '#2e44bc',
+    color: '#ffffff',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  answerButton: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 30,
+    backgroundColor: 'white',
+    margin: 10,
+    elevation: 5,
+    shadowColor: 'rgba(0, 0, 0, 0.4)',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+  },
+  buttonText: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 15,
+    textAlign: 'center',
+    textTransform: 'lowercase',
+  },
+});
--- src/screens/QuizScreen.tsx
+++ src/screens/QuizScreen.tsx
@@ -1,33 +1,22 @@
 import React, { FC, useEffect, useState } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { Props, RouteButton } from '../../types/type';
+import { View, StyleSheet } from 'react-native';
+import { Props } from '../../types/type';
 import { shuffle } from 'lodash';
 import { questions } from '../quiz/IntroductionToMedicine';
-import { TOTAL_QUESTIONS, examMenuButton } from '../contents';
+import { TOTAL_QUESTIONS } from '../contents';
 import { ResultScreen } from './ResultScreen';
 import { showCorrectAnswerAlert, showIncorrectAnswerAlert } from '../function';
-import { useNavigation } from '@react-navigation/native';
+import { TimeOverScreen } from './TimeOverScreen';
+import QuizQuestionCard from './QuizQuestionCard';
 
 export const QuizScreen: FC<Props> = () => {
   const [index, setIndex] = useState<number>(0);
   const [score, setScore] = useState<number>(0);
   const [showResultScreen, setShowResultScreen] = useState<boolean>(false);
-  const [timer, setTimer] = useState<number>(60);
+  const [timer, setTimer] = useState<number>(10);
 
   const [shuffledQuestions, setShuffledQuestions] = useState(
     shuffle(questions).slice(0, TOTAL_QUESTIONS)
-  );
-
-  const min = Math.floor(timer / 60);
-  const rem = timer % 60;
-
-  const navigation = useNavigation();
-  const handleNavigation = (navigationName: string) => {
-    navigation.navigate(navigationName as never);
-  };
-  const byFieldMenuButtons = examMenuButton.filter(
-    (button: RouteButton) => button.navigationName === 'ByField'
   );
 
   useEffect(() => {
@@ -61,6 +50,17 @@ export const QuizScreen: FC<Props> = () => {
   };
 
   return (
+    <View style={styles.container}>
+      {showResultScreen ? (
+        <ResultScreen score={score} />
+      ) : timer > 0 ? (
+        <QuizQuestionCard
+          shuffledQuestions={shuffledQuestions}
+          index={index}
+          timer={timer}
+          answerButton={answerButton}
+        />
+      ) : (
         <TimeOverScreen
           score={score}
           setScore={setScore}
@@ -69,109 +69,15 @@ export const QuizScreen: FC<Props> = () => {
           setIndex={setIndex}
           setShuffledQuestions={setShuffledQuestions}
         />
+      )}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  linearGradient: {
-    flex: 1,
-  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  returnContainer: {
-    alignItems: 'flex-end',
-    marginRight: 20,
-  },
-  byFieldButton: {
-    backgroundColor: '#2d82a8',
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    borderRadius: 10,
-  },
-  questionNumber: {
-    textAlign: 'center',
-    position: 'absolute',
-    top: 5,
-    margin: 5,
-  },
-  questionText: {
-    fontSize: 15,
-    textAlign: 'center',
-    padding: 5,
-  },
-  answerButton: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 30,
-    backgroundColor: 'white',
-    margin: 10,
-    elevation: 5,
-    shadowColor: 'rgba(0, 0, 0, 0.4)',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 6,
-  },
-  buttonText: {
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 15,
-    textAlign: 'center',
-    textTransform: 'lowercase',
-  },
-  titleQuestion: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'relative',
-    width: 360,
-    height: 205,
-    borderRadius: 15,
-    backgroundColor: 'white',
-    fontSize: 15,
-    textAlign: 'center',
-    textTransform: 'capitalize',
-    shadowColor: '#aaa',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.5,
-    shadowRadius: 4,
-    marginBottom: 20,
-    margin: 10,
-  },
-  timerText: {
-    color: 'white',
-    fontSize: 16,
-    marginLeft: 5,
-  },
-  resultContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  resultText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  resumeButton: {
-    backgroundColor: '#27ae60',
-    padding: 15,
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-  timeRestart: {
-    marginLeft: 10,
-    marginRight: 10,
-    shadowRadius: 6,
-    backgroundColor: '#2e44bc',
-    color: '#ffffff',
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  playButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    textAlign: 'center',
   },
 });
