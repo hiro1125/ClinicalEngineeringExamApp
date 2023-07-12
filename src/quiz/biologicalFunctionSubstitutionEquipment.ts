abb49cd2a269262fc52c85fa4ffe91022da7305d
hiro1125 <hiro112538@gmail.com>
2023-07-12 16:55:47 +0900

    feat: 生体機能代行装置学(28回)のデータを作成

---
+++ src/quiz/biologicalFunctionSubstitutionEquipment.ts
@@ -0,0 +1,293 @@
+import { TAGS_TEXT } from '.';
+import { QuizData } from '../../types/type';
+
+export const biologicalFunctionSubstitutionEquipment: QuizData[] = [
+  /** 28回(AM) */
+  {
+    question:
+      '吸着型酸素濃縮装置で誤っているのはどれか。\na. 酸素供給量は最大で15L/分程度である \nb. 100%の濃度の酸素を供給できる \nc. アルミケイ酸塩で窒素を吸着する \nd. サージタンクに貯蔵してから供給する \ne. 加圧空気を流して吸着剤に窒素を吸着させる',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 0,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '加温加湿器と比較して人工鼻が優れているのはどれか。\na. 死腔がない \nb. 気道出血時に適する \nc. 過剰加湿にならない \nd. 細菌汚染が少ない \ne. ネブライザとの併用に適する',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question: 'パルスオキシメータによる計測に影響を与えないのはどれか。',
+    options: [
+      '高体温',
+      '緑色のマニキュア',
+      '一酸化炭素ヘモグロビン',
+      'メトヘモグロビン',
+      'メチレンブルー静注',
+    ],
+    answerIndex: 0,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question: 'NPPVの適応になるのはどれか。',
+    options: [
+      '培疾排出国難を伴うCOPD急性増悪',
+      'ショックを呈する心原性肺水腫',
+      '呼吸停止を来した瑞息発作',
+      '免疫不全を伴った軽度のARDS',
+      '呼吸筋麻庫を来した筋萎縮性側索硬化症',
+    ],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question: '高気圧酸素治療装置内に持ち込めるのはどれか。',
+    options: [
+      'カイロ',
+      '電気アンカ',
+      '木綿のハンカチ',
+      '合成繊維の衣類',
+      'セルロイド製品',
+    ],
+    answerIndex: 2,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question: '膜型人工肺について正しいのはどれか。',
+    options: [
+      '送入ガス流量を増やすとPaC02は低下する',
+      '送入ガス酸素濃度を上げるとPaC02は低下する',
+      'ポリプ口ピレン中空糸膜は親水性である',
+      '中空糸膜型の内部灌流型では中空糸内部を送入ガスが流れる',
+      '中空糸膜型では外部灌流型の方が内部灌流型よりも圧力損失が高い',
+    ],
+    answerIndex: 0,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '人工心肺装置について誤っている組合せはどれか。\na.ベント回路 -------- 心内圧減圧 \nb.冠灌流回路 -------- 心筋保護液注入 \nc.遠心ポンプ -------- 心腔内出血回収 \nd.血液濃縮器 -------- 余剰赤血球掠去 \ne.動脈フィルタ -------- 微小気泡・栓子除去',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question: 'ヘモグロビンの酸素解離曲線について正しいのはどれか。',
+    options: [
+      '酸素含量と酸素分圧の関係を表した曲線である',
+      'アシドーシスにより右方移動する',
+      '低体温により右方移動する',
+      '低二酸化炭素血症により右方移動する',
+      '3-DPGの低下により右方移動する',
+    ],
+    answerIndex: 1,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '遠心ポンプの操作で正しいのはどれか。\na. 離脱前の低流量時には回転数による流量制御が困難である \nb. 誤って空気を体内に送り込むことはない \nc. 人工心肺運転中の送血回路の遮断は禁忌である \nd. 冷却時に流量を維持するには回転数をあげる必要がある \ne. 人工心肺停止時には送血回路を釦子で遮断し血液逆流を防ぐ',
+    options: ['a b c', ' a b e', 'a d e', 'b c d', 'c d e'],
+    answerIndex: 2,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      'PCPSについて正しいのはどれか。\na. 全身麻酔を必要とする \nb. 左心系の後負荷を軽減する \nc. 肺塞栓症によるショック時に用いられる \nd. 心停止に対する心肺蘇生に用いられる \ne. V-Aバイパス方式とV-Vバイパス方式がある',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '人工心肺による体外循環中の溶血の直接的原因とならないのはどれか。',
+    options: [
+      '大量吸引の持続',
+      '脱血不良',
+      '異型輸血',
+      '血液希釈',
+      '過度の加温',
+    ],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '血液透析を行うことによって是正されるのはどれか。\na. 高カリウム血症 \nb. 代謝性アシドーシス \nc. エリスロポエチン欠乏 \nd. ビタミンD欠乏 \ne. 低リン血症',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 0,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question: '血球成分除去療法の適応で正しいのはどれか。',
+    options: [
+      'エンドトキシン血症',
+      '透析アミロイド症',
+      '閉塞性動脈硬化症',
+      '重症筋無力症',
+      '潰瘍性大腸炎',
+    ],
+    answerIndex: 4,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      'アルブミンとグロプリン分画の分離に利用される血液浄化器はどれか。',
+    options: [
+      '血液濾過器',
+      '血液透析濾過器',
+      '血漿分離器',
+      '血漿成分分画器',
+      '血液吸着器',
+    ],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '一般に市販されている血液透析用透析液の組成[mEq/L]で誤っているのはどれか。',
+    options: ['Na+ : 140', 'K+ : 6.0', 'Ca : 3.0', 'HC0 : 30', 'Mg : 1.0'],
+    answerIndex: 1,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question: '透析装置（コンソール）に組込まれていないのはどれか。',
+    options: [
+      '電導度計',
+      '気泡検出器',
+      '透析液温計',
+      '除水制御装置',
+      '透析液浸透圧計',
+    ],
+    answerIndex: 4,
+    tags: [TAGS_TEXT.bfse],
+  },
+  /** 28回(PM) */
+  {
+    question:
+      'VCV (volume control ventilation）施行中に気道内圧上昇を来すのはどれか。\na. カフリーク \nb. 片肺挿管 \nc. 気管支痙攣 \nd. ファイティング \ne. 肺コンブライアンス増加',
+    options: ['a b c', ' a b e', 'a d e', 'b c d', 'c d e'],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question: '高気圧酸素治療について正しいのはどれか。',
+    options: [
+      '結合型酸素量は酸素分圧に比例して増大する',
+      '0ATA、100%酸素における肺抱酸素分圧は1.0ATAの1.7倍となる',
+      '溶解型酸素量よりも結合型酸素量の増大による効果が大きい',
+      '2.8ATA、100%酸素における溶解型酸素量は安静時分時酸素需要量を上回る',
+      '減圧症への有効性は示されていない',
+    ],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '吸気終末停止（EIP）で正しいのはどれか。\na. 補助換気で使用できない \nb. PCV (pressure control ventilation)で設定する \nc. 吸気時間の5%程度に設定する \nd. 不均等換気が是正できる \ne. 静肺コンプライアンスを推定できる',
+    options: ['a b c', ' a b e', 'a d e', 'b c d', 'c d e'],
+    answerIndex: 4,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      'FI020.7でPaO2150mmHg、PaCO240mmHgの時、およそのA-aD02[mmHg]はどれか。ただし、大気圧を760mmHgとする。',
+    options: ['100', '200', '300', '400', '500'],
+    answerIndex: 2,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '吸気側回路に組み込まないのはどれか。\na. 人工鼻 \nb. カプノメータ \nc. 温度センサ \nd. ウォータートラップ \ne. バクテリアフィルタ',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 0,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '人工心肺中の限外濾過による血液濃縮器について正しいのはどれか。\na. 内部灌流型の装置である \nb. メインの送脱血回路に直列に組み込む \nc. 疎水性の多孔質中空糸膜を用いる \nd. 透析液を必要とする \ne. 排出液の Na、K 濃度は細胞外液型である',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 1,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '人工心肺による体外循環中に血中濃度が低下するのはどれか。\na. ナトリウム \nb. カリウム \nc. アドレナリン \nd. グルコース \ne. サイトカイン',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 0,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '体外循環における血液希釈の目的として正しいのはどれか。\na. 血液粘性の増加 \nb. 酸素運搬能の増加 \nc. 輸血量の減少 \nd. 溶血の軽減 \ne. 膠質浸透圧の上昇',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '人工心肺による体外循環中の操作について誤っているのはどれか。\na. 平均動脈圧を60~80mmHgに維持する \nb. 混合静脈血酸素飽和度を70%以上に維持する \nc. ACT (activatclotting time)を200~300秒に維持する \nd. 復温時の送血温と脱血温の差を10°C以上に維持する \ne. プロタミンはヘパリン初期投与量の3~5倍を投与する',
+    options: ['a b c', ' a b e', 'a d e', 'b c d', 'c d e'],
+    answerIndex: 4,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question: 'オンライン血液透析濾過について誤っているのはどれか。',
+    options: [
+      '透析液の一部を補充液として使用する',
+      '認可された多用途透析装置を使用する',
+      'ダイアライザを使用する',
+      '定められた水質基準を満たした透析液を使用する',
+      'エンドトキシン捕捉フィルタを使用する',
+    ],
+    answerIndex: 2,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      'ダイアライザの性能指標のうち流量[mL/min]の次元をもつのはどれか。\na. ふるい係数 \nb. 限外濾過率 \nc. 総括物質移動面積係数 \nd. クリアランス \ne. 濾過係数',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '透析用原水の水処理システムで遊離塩素が主として除去されるのはどれか。',
+    options: [
+      '微粒子フィルタ',
+      '軟水化装置',
+      '活性炭吸着装置',
+      '限外濾過フィルタ',
+      '逆浸透装置',
+    ],
+    answerIndex: 2,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '自己血管内シャント（AVF）にはみられず人工血管内シャント（AVG）特有の合併症はどれか。',
+    options: ['静脈高血圧症', 'スティール症候群', '静脈瘤', '感染', '血清腫'],
+    answerIndex: 4,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '血液透析中に血圧低下をしばしば認める患者への対応で誤っているのはどれか。',
+    options: [
+      '体外限外濾過法(ECUM)を追加する',
+      '透析液ナトリウム濃度を増加させる',
+      '時間除水量を増加させる',
+      '透析液温度を低下させる',
+      '食事からのナトリウム摂取量を減少させる',
+    ],
+    answerIndex: 2,
+    tags: [TAGS_TEXT.bfse],
+  },
+  {
+    question:
+      '血液透析回路への空気侵入の原因となるのはどれか。\na. 抗凝固薬注入ラインの外れ \nb. 透析膜破損による血液漏出 \nc. 動脈側穿刺針と回路の接続不良 \nd. 補液ラインの閉鎖忘れ \ne. 静脈側穿刺針と回路の接続不良',
+    options: ['a b', 'a e', 'b c', 'c d', 'd e'],
+    answerIndex: 3,
+    tags: [TAGS_TEXT.bfse],
+  },
+];
